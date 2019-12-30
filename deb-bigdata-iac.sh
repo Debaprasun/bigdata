edce8598d98b03b8cd1e16b37db6d60df30e8a6b
EC2 Default User <ec2-user@ip-172-31-82-220.ec2.internal>
2019-12-30 20:39:51 +0000

    First code going in

---
+++ deb-bigdata-iac.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+#ent=$1
+#prj=$2
+#tech=$3
+
+
+for argument in "$@"
+do
+	key=$(echo $argument | cut -f1 -d=)
+	value=$(echo $argument | cut -f2 -d=)
+
+	if [[ "$key" == "ent" ]]
+	then
+		ent=$value
+	else
+		if [[ "$key" == "prj" ]]
+		then
+			prj=$value
+		else
+			if [[ "$key" == "tech" ]]
+			then
+				tech=$value
+			fi
+		fi
+	fi
+done
+
+
+#Create S3 Bucket
+# TO DO - Check for existence before creating, moving on...
+# One way is to use grep on output from aws s3 ls, and then check $? for grep command
+# And then use if [[.... else construct on that valueneccccbknjvhtjtivfgvuuuehjldtjbendirkittuece
+aws s3 mb s3://$ent-$prj-$tech-s3bucket
+
+#Remove S3 Bucket
+# aws s3 rb s3://$ent-$prj-$tech-s3bucket
